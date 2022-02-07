9faa186a6fcea035578d691bb0faa37356d1268b
tamonmon0417 <tamonmon0417@gmail.com>
2022-02-07 23:04:30 +0900

    fin sec1

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
   devServer: {
-    port: 8081,
+    port: 8080,
   },
   plugins: [
     new HtmlWebpackPlugin({
